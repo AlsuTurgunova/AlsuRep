039221bd876624c988c0c7e32a94521ee9970b10
AlsuTurgunova <alsu.turgunova@mail.ru>
2024-05-26 15:15:20 -0400

    Добавлен комментарий

--- dfs.py
+++ dfs.py
@@ -61,7 +61,7 @@ def depth_first_search(graph, start, end):
           path_lengths[neighbor] = path_lengths[node] + 1  # Обновляем длину пути
 
   return -1  # Если путь не найден, возвращаем -1
-
+#Комментарий для проверки
 # Пример использования
 graph = {
   2: [4],
